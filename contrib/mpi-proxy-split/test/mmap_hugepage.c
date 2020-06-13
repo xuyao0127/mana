5f85e7e3b7c7eee2ff0f39761d8c3488e7579983
ItsPrashant <chouhan.p@husky.neu.edu>
2020-06-13 15:49:51 -0700

    Updated mmap_hugepages test file

--- contrib/mpi-proxy-split/test/mmap_hugepage.c
+++ contrib/mpi-proxy-split/test/mmap_hugepage.c
@@ -1,18 +1,42 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <mpi.h>
 #define NUM_PAGES 100
 
 int main(){
   void *addr;
   int length=1024*1024*4;
-  for(int i=0;i<NUM_PAGES;i++){
-    addr = mmap(NULL, length, PROT_READ|PROT_WRITE,
-    	        MAP_PRIVATE|MAP_ANONYMOUS|MAP_HUGETLB, -1, 0);
-    if (addr == MAP_FAILED){
-      printf("mmap_failed");
-    }
+  int fd;
+  struct stat statbuf;
+  fd=open("mmap_test_file",O_RDONLY);
+  if(fd < 0){
+    printf("open() failed\n");
   }
-  sleep(600); 
+   /* find size of input file */
+  if (fstat (fd,&statbuf) < 0)
+  {
+    printf ("fstat error");
+    return 0;
+  }
+  MPI_Init(NULL,NULL);
+  //printf("fstat.length: %d",statbuf.st_size);
+  /*
+  for(int i=0;i<NUM_PAGES;i++){
+    addr = mmap(NULL, statbuf.st_size, PROT_READ,
+    	        MAP_PRIVATE|MAP_HUGETLB, fd, 0);
+    if (addr == MAP_FAILED){
+      printf("mmap_failed\n");
+    }
+    else{
+      printf("mmap successful: %d\n",i);
+    }
+    fflush(stdout);
+    sleep(5);
+  }
+  */
 }
